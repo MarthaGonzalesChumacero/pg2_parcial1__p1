8e7f3c7ce14bde34176e305f866bc5c4f42237b0
Martha Gonzales Chumacero <marthagonzales.sis24ch@tecba.edu.bo>
2025-05-08 22:41:52 -0400

    cambios

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # pg2_parcial1__p1
+## EXAMEN1
+
+## CAPTURAS
+____
+
+## Imagen1
+![Imagen 1](Imagenes/Imagen1.png)
+____
+
+## Imagen2
+![Imagen 2](Imagenes/Imagen2.png)
--- conversordistancia.py
+++ conversordistancia.py
@@ -2,5 +2,6 @@ from conversorbasico import conversordedistancia
 
 conv = conversordedistancia()
 
+
 conv.metros_a_centimetros(12.5)  
 conv.centimetros_a_kilometros(520000) 
